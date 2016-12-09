d5bb5c5787ae7ab354b377d6fc0c4295887eb5d1
Timothy R. Johnson <trobinj@gmail.com>
2016-12-09 12:59:26 -0800

    added algalinhibit data

--- R/larkspur.R
+++ R/larkspur.R
@@ -1,8 +1,6 @@
-#' Sample of pale white larkspur abundance.
+#' Sample of transects of pale white larkspur.
 #'
-#' Sample of 20 one-meter belt transects to estimate the abundance of pale white larkspur in a given
-#' region. The total number of sampling units in the sampling frame is 150 belt transects with a total
-#' area of 5897 square meters.
+#' Sample of 20 one-meter belt transects to estimate the abundance of pale white larkspur in a given region. The total number of sampling units in the sampling frame is 150 belt transects with a total area of 5897 square meters.
 #'
 #' @format A data frame with 20 observations and two variables:
 #' \describe{
@@ -10,12 +8,9 @@
 #'    \item{area}{sampled transect area in square meters}
 #' }
 #'
-#' @note Although not stated explicitly in the source, the sample was presumably obtained using
-#' simple random sampling.
+#' @note Although not stated explicitly in the source, the sample was presumably obtained using simple random sampling.
 #'
-#' @source Stehman, S. V., & Salzer, D. W. 2000. Estimating density from surveys employing
-#' unequal-area belt transects. \emph{Wetlands}, \emph{20}, 512-519.
+#' @source Stehman, S. V., & Salzer, D. W. 2000. Estimating density from surveys employing unequal-area belt transects. \emph{Wetlands}, \emph{20}, 512-519.
 #'
-#' Gregoire, T. G., & Valentine, H. T. (2007). \emph{Sampling strategies for natural resources
-#' and the environment}. Boca Raton, FL: Chapman & Hall/CRC.
+#' Gregoire, T. G., & Valentine, H. T. (2007). \emph{Sampling strategies for natural resources and the environment}. Boca Raton, FL: Chapman & Hall/CRC.
 "larkspur"
