eade9ef5b674736970c2069b970bd4f396c12003
trjohns <trjohns>
2016-12-11 07:27:13 -0800

    add kidneystones

---
+++ R/kidneystones.R
@@ -0,0 +1,16 @@
+#' Comparison of treatments for kidney stones. 
+#'
+#' Data from an observational study that compared the effectiveness of several treatments for kidney stones. 
+#'
+#' @format A data frame with 12 observations and four variables:
+#' \describe{
+#'    \item{size}{kidney stone size (less than 2cm or greater than or equal to 2cm)}
+#'    \item{treatment}{treatment method as a combination of nephrolithotomy and pyelolithotomy (nephr/pyelo), pyelolithotomy (pyelo), ureterolithotomy (uretero), percutaneous nephrolithotomy (perc/nephro), ESWL (extracorporeal shock wave lithotripsy), or percutaneous nephrolithotomy with extracorporeal shock wave lithotripsy (perc/nephro/ESWL)}
+#'    \item{success}{number of successful treatments}
+#'    \item{total}{total number of treatments}
+#' }
+#'
+#' @note These data have been used to illustrate Simpson's paradox by comparing percutaneous nephrolithotomy with the open surgical methods (i.e., nephrolithotomy and pyelolithotomy, pyelolithotomy, and ureterolithotomy) grouped into a single treatment condition. The paradox is observed when the methods are compared with and without controlling for stone size. See \url{https://en.wikipedia.org/wiki/Simpson's_paradox}. 
+#'
+#' @source Charig, C. R., Webb, D. R., Payne, S. R., & Wickham, J. E. A. (1986). Comparison of treatment of renal calculi by open surgery, percutaneous nephrolithotomy, and extracorporeal shockwave lithotripsy. \emph{British Medical Journal}, \emph{292}, 879-882.
+"kidneystones"
