ebf5276c89719b86db3349752bc111906e799bd6
trjohns <trjohns>
2017-01-21 10:20:02 -0800

    fixes to lincon

--- R/lincon.R
+++ R/lincon.R
@@ -7,20 +7,23 @@
 #' @param level Confidence level in (0,1).
 #' @param cnames Vector of names for the linear combination(s). 
 #' @param fcov Function for estimating the variance-covariance matrix of the model parameters.
+#' @param ... Arguments to pass to \code{fcov}.
 #' 
 #' @details For a regression model with parameters \eqn{\beta_0, \beta_1, \dots, \beta_p} a linear combination is defined as \eqn{a_0\beta_0 + a_1\beta_1 + \cdots + a_p\beta_p}. Inferences are based on either exact or Wald test statistics and confidence intervals. The estimated standard error(s) of the linear combinations are computed using any specified function for estimating the variance-covariance matrix of the model parameters. 
 #' 
 #' @export
-lincon <- function(model, a, level = 0.95, cnames = NULL, fcov = vcov, ...) {
+lincon <- function(model, a, level = 0.95, fcov = vcov, ...) {
   if (class(model) != "lm") stop("function currently only works for lm objects")
-  se <- sqrt(diag(t(a) %*% fcov(model) %*% a))
+  if (is.vector(a)) a <- matrix(a, nrow = 1)
+  se <- sqrt(diag(a %*% fcov(model, ...) %*% t(a)))
   pe <- a %*% coef(model)
   df <- summary(model)$df[2]
   lw <- pe - qt(level + (1 - level)/2, df) * se
   up <- pe + qt(level + (1 - level)/2, df) * se 
   ts <- pe/se
   pv <- 2*pt(-abs(ts), df)
-  out <- data.frame(estimate = pe, se = se, t = ts, df = df, pv = pv, lower = lw, upper = up)
-  rownames(out) <- cnames
+  out <- cbind(pe, se, ts, pv, lw, up)
+  colnames(out) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)", "lower", "upper")
+  rownames(out) <- apply(a, 1, paste, collapse = ",")
   return(out)
 }
