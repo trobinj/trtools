2667ad9505e968f7d4ce0dc050c84b82dcf2ee2d
trobinj <trobinj@gmail.com>
2017-02-16 15:21:45 -0800

    added acetazolamide

---
+++ R/acetazolamide.R
@@ -0,0 +1,17 @@
+#' Effect of acetazolamide on forelimb malformation.  
+#'
+#' Data from an experiment investigating the effect of acetazolamide on forelimb malformation in mice. Mouse fetuses were injected with acetazolamide on the 9th day of gestation. The number of malformed forelimbs for each fetus was observed on the 18th day of gestation. 
+#'
+#' @format A data frame with 588 observations and two variables:
+#' \describe{
+#'    \item{dose}{dose (mg/kg)}
+#'    \item{limbs}{number of malformed limbs}
+#' }
+#'
+#' @note The fetuses may have been nested within litters. Litter membership of each fetus, however, is unknown. The data are from Holmes, Kawanishi, and Munz (1988) and are featured in Ramsey and Schafer (1997). 
+#'
+#' @source L. B. Holmes, Kawanishi, H., & Munz, A. (1988). Acetazolamide: Maternal toxicity, pattern of malformations, and litter effect. \emph{Taratology}, \emph{37}, 335-342.
+#' 
+#' Ramsey, F. L. & Schafer, D. W. (1997). \emph{The statistical sleuth: A course in methods of data analysis}. Belmont, CA: Duxbury. 
+#' 
+"acetazolamide"
