044d28b713310af0b41bc9b2dd3319090690fcd0
trjohns <trjohns>
2017-02-26 09:50:34 -0800

    added accidents data

---
+++ R/accidents.R
@@ -0,0 +1,17 @@
+#' Traffic accident rate.
+#'
+#' Data from an observational study that recorded the number of accidents at eight locations before and after installation of traffic control measures.  
+#'
+#' @format A data frame 16 observations and four variables:
+#' \describe{
+#'    \item{accidents}{number of accidents}
+#'    \item{years}{number of years of observation}
+#'    \item{location}{monitored location (one of eight)}
+#'    \item{treatment}{before or after introduction of traffic control measures}
+#' }
+#'
+#' @note The data are from an example from Pawitan (2013). No other source is given so the data are assumed to be artificial. 
+#'
+#' @source Pawitan, Y. (2013). \emph{In all likelihood: Statistical modeling and inference using likelihood}. Oxford University Press. 
+#' 
+"accidents"
