334e3e71095bb4d3faac142ea64efbf1d6771ef1
trobinj <trobinj@gmail.com>
2017-03-11 10:22:11 -0800

    doc changes to glmint

--- R/glmint.R
+++ R/glmint.R
@@ -1,16 +1,19 @@
-#' Estimated expected response and confidence intervals for (generalized) linear models (experimental).
+#' Confidence intervals for the estimated expected response of generalized linear models (experimental).
 #' 
-#' This function computes the estimated expected response and the confidence interval for the estimated expected response for (generalized) linear models 
+#' This function computes the estimated expected response and the confidence interval for the estimated expected response for (generalized) linear models. 
 #' 
 #' @param object Model object of class \code{glm}.
 #' @param newdata An optional data frame in which to look for variables with which to predict. If omitted the data frame used in creating the model object is used. 
 #' @param df Degrees of freedom for the confidence interval. If omitted it is extracted from the model object.
 #' @param level Confidence level in (0,1).
 #' 
+#' @details This function simplifies the process of computing the endpoints of the confidence interval for the expected response on the scale of the response variable for generalized linear models using the \code{predict} function with \code{se.fit = TRUE}. It computes the Wald confidence interval on the scale of the linear predictor and then uses the inverse of the link function to map the interval to the scale of the response variable. 
+#' 
 #' @examples 
 #' m <- glm(cbind(y,total-y) ~ density + species, family = quasibinomial, data = rotifer)
 #' d <- expand.grid(density = seq(1.01, 1.07, by = 0.01), species = c("kc","pm"))
 #' glmint(m, newdata = d)
+#' 
 #' @importFrom stats predict
 #' @export
 glmint <- function(object, newdata, df, level = 0.95) {
