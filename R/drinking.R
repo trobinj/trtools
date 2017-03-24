cbab9061bc4f09b1a42943ad0d67cc1f0937eabc
Timothy R. Johnson <trobinj@gmail.com>
2017-03-24 10:48:50 -0700

    added drinking data

---
+++ R/drinking.R
@@ -0,0 +1,9 @@
+#' Hypothetical data for demonstrating Poisson zero-inflation.  
+#' 
+#' @format A data frame with 1400 observations and three variables:
+#' \describe{
+#'    \item{sex:}{sex of the respondent (male or female)}
+#'    \item{age:}{age of the respondent (years)}
+#'    \item{drinks:}{number of reported alcoholic drinks in the last week}
+#' }
+"drinking"
--- R/jejunalcrypt.R
+++ R/jejunalcrypt.R
@@ -8,8 +8,6 @@
 #'    \item{crypts:}{number of surviving jejunal crypts}
 #' }
 #'
-#' @note A complication with these data is that the number of jejnual crypts prior to exposure to radiation is unknown and cannot be determined postmortem. The distribution of the number of surviving crypts given the dose appears to exhibit \emph{underdispersion} if a Poisson distribution is assumed.  
-#'
 #' @source Kim, D. K. & Taylor, J. M. G. (1994). Transform-both-sides approach for overdispersed binomial data when N is unobserved. \emph{Journal of the American Statisical Association}, \emph{89}, 833-845.
 #' 
 #' Faddy, M. J. & Smith, D. M. (2011). Analysis of count data with covariate dependence in both mean and variance. \emph{Journal of Applied Statistics}, \emph{38}, 2683-2694.
