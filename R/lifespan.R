f0c592ae2f017c88c3d0b6d42c8e37aed0b94665
trobinj <trobinj@gmail.com>
2017-03-30 09:20:46 -0700

    added lifespan

---
+++ R/lifespan.R
@@ -0,0 +1,13 @@
+#' Lifespan of humans and dogs.
+#'
+#' Hypothetical data of the lifespan of humans and dogs for illustrating accelerated failure time models. The lifespan of humans is about seven times that of dogs.
+#'
+#' @format A data frame 500 observations and two variables:
+#' \describe{
+#'    \item{years:}{lifespan in year (i.e., years from birth to death)}
+#'    \item{species:}{species (dog or human)}
+#' }
+#'
+#' @note Some of the extreme survival times are obviously not realistic. 
+#' 
+"lifespan"
