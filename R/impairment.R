9f4caaf507e5ceff35ac7f11c52bd0eb01539e30
trjohns <trjohns>
2017-04-09 10:42:08 -0700

    added impairment data

---
+++ R/impairment.R
@@ -0,0 +1,11 @@
+#' Degree of mental impairment. 
+#'
+#' Degree of mental impairment as a function of socioeconomic status and life events index.
+#'
+#' @format A data frame of 40 observations and three variables:
+#' \describe{
+#'    \item{impairment:}{degree of mental impairment (none, mild, moderate, impaired)}
+#'    \item{ses:}{socioeconomic status (low or high)}
+#'    \item{events:}{life events index score}
+#' }
+"impairment"
