5c35e32c820e88cfc129cd64fa2f0e472073e939
trobinj <trobinj@gmail.com>
2017-04-12 15:09:48 -0700

    added bumpus data

---
+++ R/bumpus.R
@@ -0,0 +1,26 @@
+#' House sparrow survival data. 
+#'
+#' Measurements of samples of house sparrows (Passer domesticus) brought to the Anatomical Laboratory at Brown University after a severe storm. They were published by Bumpus (1898) who used the data to illustrate natural selection. 
+#'
+#' @format A data frame of 136 observations and 13 variables:
+#' \describe{
+#'    \item{line:}{line number from Bumpus' table}
+#'    \item{sex:}{male (m) or female (f)}
+#'    \item{age:}{adult (a) or young (y) for males only}
+#'    \item{survival:}{logical indicating survival of the storm}
+#'    \item{length:}{length (mm) from "beak to the tip of the tail"}
+#'    \item{alar:}{alar extent (mm) from "tip to tip of the extended wings"}
+#'    \item{weight:}{weight in grams}
+#'    \item{head:}{length of beak and head (mm) "from tip of the beak to the occiput"}
+#'    \item{humerus:}{length (in) of the humerus}
+#'    \item{femur:}{length (in) of the femur}
+#'    \item{tibiotarsus:}{length (in) of the tibiotarsus}
+#'    \item{width:}{width (in) of skull "from the postorbital bone of one side to the postorbital bone of the other"}
+#'    \item{keel:}{length (in) of keel of the sternum}
+#' }
+#'
+#' @note The data and variable descriptions were retrieved from https://www.fieldmuseum.org/science/blog/hermon-bumpus-and-house-sparrows.
+#'
+#' @source Bumpus, H. C. (1898). Eleventh lecture. The elimination of the unfit as illustrated by the introduced sparrow, Passer domesticus. (A fourth contribution to the study of variation.) Biological Lectures: Woods Hole Marine Biological Laboratory, 209-225. 
+#' 
+"bumpus"
