ff7c289d7e9f9f8dc4dab4a85167423aef72bf40
Timothy R. Johnson <trobinj@gmail.com>
2017-04-14 14:08:39 -0700

    added aberrations

---
+++ R/aberrations.R
@@ -0,0 +1,16 @@
+#' Chromosome aberrations in survivors of Hiroshima and Nagasaki. 
+#'
+#' Study of the number of aberrations in a sample of 100 cells in survivors of the atomic bombs dropped on Hiroshima and Nagasaki.  
+#'
+#' @format A data frame 1052 observations and three variables:
+#' \describe{
+#'    \item{cells:}{number of cells (out of 100) that exhibited chromosome aberrations}
+#'    \item{radiation:}{approximate dose of radiation (in rads) }
+#'    \item{city:}{city of residence}
+#' }
+#'
+#' @note The data are from Otake and Prentice (1984). The 400-999 and 1000+ rad groups have been combined here as both are quite sparse. 
+#'
+#' @source Otake, M. & Prentice, R. L. (1984). The analysis of chromosomally aberrant cells based on beta-binomial distribution. \emph{Radiation Research}, \emph{98}, 456-470.
+#' 
+"aberrations"
