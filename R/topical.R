dc63d3d90b7f6a36dec7464c9d03db2831906798
Timothy R. Johnson <trobinj@gmail.com>
2017-04-26 13:39:31 -0700

    added topical

---
+++ R/topical.R
@@ -0,0 +1,18 @@
+#' Efficacy of two topical cream preparations. 
+#'
+#' Data from a multicenter randomized clinical trial of two topical cream preparations. 
+#'
+#' @format A data frame 16 observations and five variables:
+#' \describe{
+#'    \item{clinic:}{clinic where the creams were tested}
+#'    \item{treatment:}{topical cream preparation (active drug or control)}
+#'    \item{favorable:}{number of favorable results}
+#'    \item{unfavorable:}{number of unfavorable results}
+#'    \item{total:}{total number of preparations}
+#' }
+#'
+#' @note The data are used by Beitler and Landis (1985) to illustrate mixed effects models for categorical data. 
+#'
+#' @source Beitler, P. J. & Landis, J. R. (1985). A mixed-effects model for categorical data. \emph{Biometrics}, \emph{41}, 991-1000. 
+#' 
+"topical"
