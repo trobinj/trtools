e36c72ec47a14a70285a43abfd64593dccfa632a
Timothy R. Johnson <trobinj@gmail.com>
2017-08-26 08:12:22 -0700

    added reciprocity

---
+++ R/reciprocity.R
@@ -0,0 +1,16 @@
+#' Generalized reciprocity in rats. 
+#'
+#' Data from a study investigating generalized reciprocity in wild-type rats (Rattus norvegicus). The rats were trained to pull a stick to produce food for an unfamiliar partner rat. The rats were then put into two conditions in a cross-over design. In one condition the focal rat received help from other rats, while in the other condition they did not. The treatment order was randomized and separated by a four day interval. The number of pulls per minute was recorded for each treatment condition. 
+#'
+#' @format A data frame 19 observations and three variables:
+#' \describe{
+#'    \item{rat:}{identifier of focal rat}
+#'    \item{help:}{rate of pulls per minute after receiving help}
+#'    \item{nohelp:}{rate of pulls per minute after not receiving any help}
+#' }
+#'
+#' @note Rats with the same first digit in their identifier were from the same litter.
+#'
+#' @source Rutte, C. & Taborsky, M. (2007). Generalized reciprocity in rats. \emph{PLoS Biol}, \emph{5(7)}: e196. doi:10.1371/journal.pbio.0050196.
+#' 
+"reciprocity"
