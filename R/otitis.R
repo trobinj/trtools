1b44b1c692de66dfe785f5726b3b6e46c6655eca
Timothy R. Johnson <trobinj@gmail.com>
2018-05-10 07:46:52 -0700

    minor modifications to otitis data

--- R/otitis.R
+++ R/otitis.R
@@ -11,9 +11,9 @@
 #'    \item{count}{number of children}
 #' }
 #'
-#' @note Details concerning the source of these data can be found in Mandel et al. (1982). These data are also featured in Rosner (1989). Note that the data are formatted in aggregate form so that the \code{count} variable reflects the number of children with a given combination of values of the other variables. 
+#' @note Details concerning the source of these data can be found in Mandel et al. (1982). These data are also featured in Rosner (1989).
 #'
-#' @source Rosner, B. (1989). Multivariate methods for clustered binary data with more than one level of nexting. \emph{Journal of the American Statistical Association}, \emph{84}, 373-380.
+#' @source Rosner, B. (1989). Multivariate methods for clustered binary data with more than one level of nesting. \emph{Journal of the American Statistical Association}, \emph{84}, 373-380.
 #' 
 #' Mandel, E., Bluestone, C. D., Rockette, H. E., Blatter, M. M., Reisinger, K. S., Wucher, F. P., & Harper, J. (1982). Duration of antibiotic treatment for acute otitis media. \emph{Pediatric Infectious Diseases}, \emph{1}, 310-316.
 "otitis"
