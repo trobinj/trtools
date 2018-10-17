69ddd687e2413f85f0cc4d20426d469c2bfef2c4
Timothy R. Johnson <trobinj@gmail.com>
2018-10-17 14:37:10 -0700

    added rayleigh data

---
+++ R/rayleigh.R
@@ -0,0 +1,14 @@
+#' Lord Rayleigh's measurements of nitrogen gas.
+#' 
+#' Lord Rayleigh (John William Strutt) conducted several experiments in which he tried to isolate samples of nitrogen gas at fixed volumes, temperature, and pressure by removing oxygen from dry air or through the decomposition of nitrogen compounds.
+#'
+#' @format A data frame with 15 observations and 2 variables:
+#' \describe{
+#'    \item{mass:}{mass of gas sample in grams}
+#'    \item{method:}{method of isolating the nitrogen gas}
+#' }
+#' 
+#' @note The difference in the mass of nitrogen isolated from atmospheric air (using the "hot iron" or "ferrous hydrate" methods) with nitrogen obtained from chemical decompositions (i.e., from nitric oxide, nitrous oxide, or ammonium nitrate) is assumed to be due to the presence of other elements or compounds in the atmospheric air, namely argon. 
+#'
+#' @source Larsen, R. D. (1990). Lessons learned from Lord Rayleigh on the importance of data analysis. \emph{Journal of Chemical Education}, \emph{67(11)}, 925--928.
+"rayleigh"
