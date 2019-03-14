71335176ba41d6c8183d96717b498cef727d29ef
Timothy R. Johnson <trobinj@gmail.com>
2019-03-13 19:14:42 -0700

    added ironretention data

---
+++ R/ironretention.R
@@ -0,0 +1,14 @@
+#' Iron retention in mice.
+#'
+#' Data from a randomized experiment on the retention of ferrous (Fe2+) and ferric (Fe3+) iron administered to mice at concentrations of 10.2, 1.2, and 0.3 millimolars. The percent of iron retained was measured. 
+#'
+#' @format A data frame of 108 observations on three variables:
+#' \describe{
+#'    \item{retention:}{percent of iron administered that was retained}
+#'    \item{concentration:}{concentration of iron administered in millimolars}
+#'    \item{ion:}{type of iron (ferrious or ferric)}
+#' }
+#'
+#' @source Rice, J. A. (1998). \emph{Mathematical statistics and data analysis}. Pacific Grove, CA: Wadsworth & Brook/Cole.
+#' 
+"ironretention"
