d002db7017c2c0e6801ac7830f4be4e44c9ccf11
Timothy R. Johnson <trobinj@gmail.com>
2019-03-17 08:19:26 -0700

    added pock data

---
+++ R/pock.R
@@ -0,0 +1,13 @@
+#' Pock lesions on chick embroyos.
+#'
+#' Data from a study of the number of lesions from viruses of the pox group at several dilutions of the viral medium.
+#'
+#' @format A data frame of 58 observations on two variables.
+#' \describe{
+#'    \item{Dilution:}{dilution of the viral medium}
+#'    \item{Count:}{number of lesions}
+#' }
+#'
+#' @source Breslow, N. E. (1990). Further studies in the variability of pock counts. \emph{Statistics in Medicine}, \emph{9}, 615--626.
+#' 
+"pock"
