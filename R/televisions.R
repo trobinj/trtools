a9c7a2c2e191d25477dd0adea7e19d017144aac6
Timothy R. Johnson <trobinj@gmail.com>
2019-09-24 20:14:26 -0700

    fixed typo in televisions documentation

--- R/televisions.R
+++ R/televisions.R
@@ -2,7 +2,7 @@
 #'
 #' Survey carried out in Des Moines, Iowa, in 1951. Sampling units were clusters of households. The number of households with televisions was recorded in each selected sampling unit. The total number of households in the sampling frame was 56296, and the total number of sampling units in the frame was 9460.  
 #' 
-#' @format A data frame 132 observations and two variables:
+#' @format A data frame of 132 observations and two variables:
 #' \describe{
 #'    \item{households:}{number of households in the sampling unit}
 #'    \item{tv:}{number of households with televisions in the sampling unit}
