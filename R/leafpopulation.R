33a3ed9416f1e9b2e2382a59d2f28e46964ca407
Timothy R. Johnson <trobinj@gmail.com>
2019-09-28 10:41:14 -0700

    added leafarea, leafpopulation, and pike

---
+++ R/leafpopulation.R
@@ -0,0 +1,14 @@
+#' Weight and area of a population of leaves.
+#'
+#' Weight and area of all the leaves from a sapling. 
+#'
+#' @format A data frame with 64 observations and two variables:
+#' \describe{
+#'    \item{weight:}{leaf weight (grams)}
+#'    \item{area:}{leaf area (square cm)}
+#' }
+#'
+#' @note Data from Gregoire and Valentine (2007). These data can be used as a population to demonstrate calibration. Of particular note is the somewhat nonlinear relationship between weight and area. 
+#'
+#' @source Gregoire, T. G. & Valentine, H. T. (2007). \emph{Sampling strategies for natural resources and the environment}. Boca Raton, FL: Chapman & Hall/CRC.
+"leafpopulation"
---
+++ R/pike.R
@@ -0,0 +1,15 @@
+#' Creel surveys of Northern Pike.
+#'
+#' Data from a survey of Northern Pike (\emph{Esox lucius}) at Ventura Marsh, Iowa, in October of 1953. A total of 1146 tagged pike were first released into the system, and then 16 hauls were conducted (presumably sampling with replacement). 
+#' 
+#' @format A data frame of 16 observations and two variables:
+#' \describe{
+#'    \item{catch:}{number of fish caught}
+#'    \item{tagged:}{number of tagged fish caught}
+#' }
+#' 
+#' @note The data are from Jessen (1978). Jessen states that the data are from R. J. Muncy and R. L. Ridenhour. Note that the summary statistics for catch reported by Jessen do not quite match the data he reported.
+#' 
+#' @source Jessen, R. J. (1978). \emph{Statistical survey techniques}. Wiley.
+#' 
+"pike"
